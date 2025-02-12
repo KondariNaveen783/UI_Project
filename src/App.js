dd0b5eeca0bfc1ecbd0ae29023b1134ee91823c5
harsha <m.harshavardhanreddy239@gmail.com>
2025-02-12 17:12:55 +0530

    changes made in App component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Main/Home";
 import TopicList from "./components/Main/TopicLists";
 import TopicDetail from "./components/Main/TopicDetails";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => (
   <Router>
